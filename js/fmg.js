6e63d0d7bcc4e4559e0da4cadcf130dba2ef7a45
Chris <chris.kuiper@gmail.com>
2016-12-25 12:12:26 -0500

    resolve issue enabling submit button on required fields

--- js/fmg.js
+++ js/fmg.js
@@ -2,10 +2,10 @@
 // Country and State drop-down menues.
 const states = new Array();
 states[0]="";
-states[1]="Please Select a State|Alberta|British Columbia|Manitoba|New Brunswick|Newfoundland|Northwest Territories|Nova Scotia|Nunavut|Ontario|Prince Edward Island|Quebec|Saskatchewan|Yukon Territory";
-states[2]="Please Select a State|Alsace|Aquitaine|Auvergne|Basse-Normandie|Bourgogne|Bretagne|Centre|Champagne-Ardenne|Corse|Franche-Comte|Haute-Normandie|Ile-de-France|Languedoc-Roussillon|Limousin|Lorraine|Midi-Pyrenees|Nord-Pas-de-Calais|Pays de la Loire|Picardie|Poitou-Charentes|Provence-Alpes-Cote d'Azur|Rhone-Alpes";
-states[3]="Please Select a State|Aguascalientes|Baja California|Baja California Sur|Campeche|Chiapas|Chihuahua|Coahuila de Zaragoza|Colima|Distrito Federal|Durango|Guanajuato|Guerrero|Hidalgo|Jalisco|Mexico|Michoacan de Ocampo|Morelos|Nayarit|Nuevo Leon|Oaxaca|Puebla|Queretaro de Arteaga|Quintana Roo|San Luis Potosi|Sinaloa|Sonora|Tabasco|Tamaulipas|Tlaxcala|Veracruz-Llave|Yucatan|Zacatecas";
-states[4]="Please Select a State|Alabama|Alaska|Arizona|Arkansas|California|Colorado|Connecticut|Delaware|District of Columbia|Florida|Georgia|Hawaii|Idaho|Illinois|Indiana|Iowa|Kansas|Kentucky|Louisiana|Maine|Maryland|Massachusetts|Michigan|Minnesota|Mississippi|Missouri|Montana|Nebraska|Nevada|New Hampshire|New Jersey|New Mexico|New York|North Carolina|North Dakota|Ohio|Oklahoma|Oregon|Pennsylvania|Rhode Island|South Carolina|South Dakota|Tennessee|Texas|Utah|Vermont|Virginia|Washington|West Virginia|Wisconsin|Wyoming";
+states[1]="Alberta|British Columbia|Manitoba|New Brunswick|Newfoundland|Northwest Territories|Nova Scotia|Nunavut|Ontario|Prince Edward Island|Quebec|Saskatchewan|Yukon Territory";
+states[2]="Alsace|Aquitaine|Auvergne|Basse-Normandie|Bourgogne|Bretagne|Centre|Champagne-Ardenne|Corse|Franche-Comte|Haute-Normandie|Ile-de-France|Languedoc-Roussillon|Limousin|Lorraine|Midi-Pyrenees|Nord-Pas-de-Calais|Pays de la Loire|Picardie|Poitou-Charentes|Provence-Alpes-Cote d'Azur|Rhone-Alpes";
+states[3]="Aguascalientes|Baja California|Baja California Sur|Campeche|Chiapas|Chihuahua|Coahuila de Zaragoza|Colima|Distrito Federal|Durango|Guanajuato|Guerrero|Hidalgo|Jalisco|Mexico|Michoacan de Ocampo|Morelos|Nayarit|Nuevo Leon|Oaxaca|Puebla|Queretaro de Arteaga|Quintana Roo|San Luis Potosi|Sinaloa|Sonora|Tabasco|Tamaulipas|Tlaxcala|Veracruz-Llave|Yucatan|Zacatecas";
+states[4]="Alabama|Alaska|Arizona|Arkansas|California|Colorado|Connecticut|Delaware|District of Columbia|Florida|Georgia|Hawaii|Idaho|Illinois|Indiana|Iowa|Kansas|Kentucky|Louisiana|Maine|Maryland|Massachusetts|Michigan|Minnesota|Mississippi|Missouri|Montana|Nebraska|Nevada|New Hampshire|New Jersey|New Mexico|New York|North Carolina|North Dakota|Ohio|Oklahoma|Oregon|Pennsylvania|Rhode Island|South Carolina|South Dakota|Tennessee|Texas|Utah|Vermont|Virginia|Washington|West Virginia|Wisconsin|Wyoming";
 
 function stateList(stateId, stateIndex) {
   var stateOption = document.getElementById(stateId);
@@ -24,12 +24,36 @@ $('#country').on('change', function() {
     } else {
         $('.enable').removeAttr('disabled');
     }
-});
-
-$('#country').change(function () {
     var isRequired = /CA|US/i.test(this.value);
     $('div[name=zip]').toggleClass('required',isRequired);
 });
+
+function validation(value){
+    return (value.length) ? true : false;
+    // same as if (value.length) {return true;} else {return false;}
+}
+
+// Detecting the event for any field of class field
+$(".required").keyup(function() {
+
+    // checking each item
+    $(".required").each(function(index, item){
+
+        if(!validation($(item).val())){
+            $("#btn").prop("disabled", false);
+            return false; // break the loop. there is a validation error
+        }
+
+        // if is the last item and there aren't validation errors
+        if ($(item).attr('id') == $(".required").last().attr('id'))
+            $("#btn").prop("disabled", true);
+    });
+})
+
+// $('#country').change(function () {
+//     var isRequired = /CA|US/i.test(this.value);
+//     $('div[name=zip]').toggleClass('required',isRequired);
+// });
 
 // $('form').submit(function(){
 //     // get all empty required fields
